e7fffd3dda1f94fcfc0c6be279697c3267816349
Maximiliano Morra <morramaximiliano25@gmail.com>
2026-07-05 19:42:17 -0300

    .

--- src/components/sections/Nav.jsx
+++ src/components/sections/Nav.jsx
@@ -32,7 +32,7 @@ const Nav = () => {
     }`;
 
   return (
-    <nav className="sticky top-0 z-50 w-full h-[150px] bg-gray-950/80 backdrop-blur-md border-b border-gray-900 flex flex-col items-center">
+    <nav className="sticky top-0 z-50 w-full h-[150px] bg-gray-950/80 backdrop-blur-md border-b border-gray-900 text-center">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           <NavLink
